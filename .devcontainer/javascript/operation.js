f72c1bd28c41fa7cfa67c88de30fd1d022ad6744
rohit kumar <141267841+rkitjnp@users.noreply.github.com>
2025-08-07 04:57:59 +0000

    done

---
+++ .devcontainer/javascript/operation.js
@@ -0,0 +1,16 @@
+let value =3
+let negvalue= -value
+console.log(value)
+console.log(negvalue)
+console.log(2+2)
+console.log(5-2)
+console.log(2*2)
+console.log(2/2)
+console.log(2%2)
+console.log(2**3)
+console.log(true)
+console.log(+true)
+//console.log(true+) not possible
+//assignment operatior
+let num1, num2, num3
+num1 = num2 = num3 =3
