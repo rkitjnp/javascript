462e4e722d7c400f3c2f5f52979c707e28b1908e
rohit kumar <141267841+rkitjnp@users.noreply.github.com>
2025-08-07 04:24:06 +0000

    done

---
+++ .devcontainer/javascript/basic.js
@@ -0,0 +1,12 @@
+let a=3
+let b="34"
+console.log(a)
+console.log(b)
+console.log(typeof a)
+console.log(typeof b)
+a="54"
+b=12
+console.log(a)
+console.log(b)
+console.log(typeof a)
+console.log(typeof b)
